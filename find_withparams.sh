7bfb3a914570782adb2b8e6940bc156cf6945b3d
Tim Gresham <timothy.gresham@intel.com>
2024-11-26 21:32:41 -0800

    Update find_withparams.sh

--- find_withparams.sh
+++ find_withparams.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-# Check if the required parameters are provided
-if [[ $# -ne 4 ]]; then
-    echo "Usage: $0 <user> <remote_host> <start_dir> <search_string>"
-    exit 1
-fi
-
-# Assign input parameters to variables
-REMOTE_USER="$1"
-REMOTE_HOST="$2"
-START_DIR="$3"
-SEARCH_STRING="$4"
+# Constants
+REMOTE_USER="user"                # Replace with the remote username
+REMOTE_HOST="remote_server"       # Replace with the remote server hostname or IP
+SSH_KEY="~/.ssh/id_rsa"           # Path to your SSH private key
+START_DIR="/path/to/start"        # Directory to start the search
+SEARCH_STRING="search_string"     # String to search for in file names
 
 # Execute the find command on the remote server and get the oldest file
-ssh "${REMOTE_USER}@${REMOTE_HOST}" "
+ssh -i "${SSH_KEY}" "${REMOTE_USER}@${REMOTE_HOST}" "
     find ${START_DIR} -type f -name '*${SEARCH_STRING}*' -printf '%T@ %p\n' | 
     sort -n | 
     head -n 1 | 
