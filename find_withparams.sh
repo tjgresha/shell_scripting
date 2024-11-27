ee0a14fa6a082e479fe3338af7ef8827116d5b50
Tim Gresham <timothy.gresham@intel.com>
2024-11-27 10:31:07 -0800

    Update find_withparams.sh

--- find_withparams.sh
+++ find_withparams.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
 # Constants
-REMOTE_USER="user"                # Replace with the remote username
-REMOTE_HOST="remote_server"       # Replace with the remote server hostname or IP
+REMOTE="user@remote_server"       # username and remote
 SSH_KEY="~/.ssh/id_rsa"           # Path to your SSH private key
 START_DIR="/path/to/start"        # Directory to start the search
-SEARCH_STRING="search_string"     # String to search for in file names
+MAX_FILES=21                      # Maximum number of files to retain + the symlink to latest
 
-# Execute the find command on the remote server and get the oldest file
-ssh -i "${SSH_KEY}" "${REMOTE_USER}@${REMOTE_HOST}" "
-    find ${START_DIR} -type f -name '*${SEARCH_STRING}*' -printf '%T@ %p\n' | 
-    sort -n | 
-    head -n 1 | 
-    awk '{print \$2}'
-" || {
-    echo "Failed to execute the command on the remote server."
-    exit 1
+# Array of search strings
+SEARCH_STRINGS=("search_string1" "search_string2" "search_string3")
+
+# Function to process each search string
+process_search_string() {
+    local search_string="$1"
+
+    # SSH command to delete oldest files if count exceeds MAX_FILES
+    ssh -i "${SSH_KEY}" "${REMOTE}" "
+        while [ \$(find ${START_DIR} -type f -name '*${search_string}*' | wc -l) -gt ${MAX_FILES} ]; do
+            OLDEST_FILE=\$(find ${START_DIR} -type f -name '*${search_string}*' -printf '%T@ %p\n' | sort -n | head -n 1 | awk '{print \$2}');
+            echo \"Deleting: \$OLDEST_FILE\";
+            rm -f \"\$OLDEST_FILE\";
+        done
+    " || {
+        echo "Failed to execute the command for search string: ${search_string}."
+        exit 1
+    }
 }
+
+# Iterate through each search string
+for search_string in "${SEARCH_STRINGS[@]}"; do
+    echo "Processing files matching: *${search_string}*"
+    process_search_string "${search_string}"
+done
